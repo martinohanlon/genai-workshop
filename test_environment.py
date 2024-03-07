dc1ad32bc30670dbd9ab64aba999cb38ccaf310f
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2024-03-07 15:42:11 +0000

    improved test

--- test_environment.py
+++ test_environment.py
@@ -6,38 +6,46 @@ import unittest
 from dotenv import load_dotenv
 load_dotenv()
 
-class TestEnvironmentVariables(unittest.TestCase):
+class TestEnvironment(unittest.TestCase):
+
+    skip_openai_test = True
+    skip_neo4j_test = True
+
     def env_variable_exists(self, variable_name):
         self.assertIsNotNone(
             os.getenv(variable_name),
             f"{variable_name} not found in .env file")
 
-    def test_openai_variable(self):
+    def test_openai_variables(self):
         self.env_variable_exists('OPENAI_API_KEY')
-    
+        TestEnvironment.skip_openai_test = False
+
     def test_neo4j_variables(self):
         self.env_variable_exists('NEO4J_URI')
         self.env_variable_exists('NEO4J_USERNAME')
         self.env_variable_exists('NEO4J_PASSWORD')
-        
-class TestLangchainOpenAI(unittest.TestCase):
+        TestEnvironment.skip_neo4j_test = False
 
     def test_openai_connection(self):
-            from openai import OpenAI, AuthenticationError
+        if TestEnvironment.skip_openai_test:
+            self.skipTest("Skipping OpenAI test")
 
-            llm = OpenAI()
-            
-            try:
-                models = llm.models.list()
-            except AuthenticationError as e:
-                models = None
-            self.assertIsNotNone(
-                models,
-                "OpenAI connection failed. Check your API key in .env file.")
-            
-class TestNeo4jConnection(unittest.TestCase):
+        from openai import OpenAI, AuthenticationError
+
+        llm = OpenAI()
+        
+        try:
+            models = llm.models.list()
+        except AuthenticationError as e:
+            models = None
+        self.assertIsNotNone(
+            models,
+            "OpenAI connection failed. Check your API key in .env file.")
 
     def test_neo4j_connection(self):
+        if TestEnvironment.skip_neo4j_test:
+            self.skipTest("Skipping Neo4j connection test")
+
         from neo4j import GraphDatabase
 
         driver = GraphDatabase.driver(
@@ -57,7 +65,16 @@ class TestNeo4jConnection(unittest.TestCase):
             connected,
             "Neo4j connection failed. Check your NEO4J_URI, NEO4J_USERNAME, and NEO4J_PASSWORD in .env file."
             )
-
+        
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestEnvironment('test_openai_variables'))
+    suite.addTest(TestEnvironment('test_neo4j_variables'))
+    suite.addTest(TestEnvironment('test_openai_connection'))
+    suite.addTest(TestEnvironment('test_neo4j_connection'))
+    return suite
 
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+    
